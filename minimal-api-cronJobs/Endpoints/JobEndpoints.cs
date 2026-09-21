a92f6c1a41df988894f354f911f73d0a3b2d9346
Eric Sonego <eeericsonego@gmail.com>
2026-09-20 23:08:46 -0300

    fix: group endpoint /jobs and remove duplicate post

--- minimal-api-cronJobs/Endpoints/JobEndpoints.cs
+++ minimal-api-cronJobs/Endpoints/JobEndpoints.cs
@@ -7,40 +7,34 @@ namespace CronJobs.Endpoints
     {
         public static void MapJobEndpoints(this WebApplication app)
         {
+            var group = app.MapGroup("/jobs");
+
             // post
-            app.MapPost("/jobs", async (JobModel job, JobService service) =>
+            group.MapPost("/", async (JobModel job, JobService service) =>
             {
+                if (!JobService.IsValidSchedule(job.Schedule))
+                    return Results.BadRequest("invalid schedule. use the format cron, ex.: */5 * * * *");
+
                 var createdJob = await service.CreateJob(job);
                 return Results.Created($"/jobs/{createdJob.IdJob}", createdJob);
             });
 
             // get
-            app.MapGet("/jobs", async (JobService service) =>
+            group.MapGet("/", async (JobService service) =>
             {
                 var jobs = await service.GetJobs();
                 return Results.Ok(jobs);
             });
 
             // get {id}
-            app.MapGet("/jobs/{id}", async (int id, JobService service) =>
+            group.MapGet("/{id:int}", async (int id, JobService service) =>
             {
                 var job = await service.GetJobById(id);
-                if (job is null)
-                    return Results.NotFound();
-                return Results.Ok(job);
+                return job is null ? Results.NotFound() : Results.Ok(job);
             });
 
-
-            app.MapPost("/", async (JobModel job, JobService service) =>
-            {
-                if (!JobService.IsValidSchedule(job.Schedule))
-                    return Results.BadRequest("invalid schedule. use the format cron, ex.: */5 * * * *");
-
-                var criado = await service.CreateJob(job);
-                return Results.Created($"/jobs/{criado.IdJob}", criado);
-            });
-
-            app.MapPut("/{id:int}", async (int id, JobModel input, JobService service) =>
+            // put
+            group.MapPut("/{id:int}", async (int id, JobModel input, JobService service) =>
             {
                 if (!JobService.IsValidSchedule(input.Schedule))
                     return Results.BadRequest("invalid schedule. use the format cron, ex.: */5 * * * *");
@@ -48,7 +42,8 @@ namespace CronJobs.Endpoints
                 return await service.UpdateJob(id, input) ? Results.NoContent() : Results.NotFound();
             });
 
-            app.MapDelete("/{id:int}", async (int id, JobService service) =>
+            // delete
+            group.MapDelete("/{id:int}", async (int id, JobService service) =>
                 await service.DeleteJob(id) ? Results.NoContent() : Results.NotFound());
         }
     }
